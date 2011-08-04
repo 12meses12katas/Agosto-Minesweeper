e654927882204b18a56728fba76cb8617a83229b
Guillermo Gutiérrez <guillermo.gutierrez@donewtech.com>
2011-08-04 17:19:38 +0200

    REFACTOR

--- ggalmazor/python/test/mine_sweeper_tests.py
+++ ggalmazor/python/test/mine_sweeper_tests.py
@@ -1,13 +1,18 @@
 import unittest
 
+EOF = "0 0"
+
 class mine_sweeper_tests(unittest.TestCase):
+    def isHeader(self, line):
+        return line.count(" ") > 0
+
     def test_sabe_cuando_hay_un_campo_definido(self):
         fields = "1 1\n.\n0 0"
 
         field_count = 0
         for line in fields.split("\n"):
-            if (line.count(" ") > 0):
-                if ("0 0" != line):
+            if (self.isHeader(line)):
+                if (EOF != line):
                     field_count += 1
 
         self.assertEquals(1, field_count)
