137a48f7bbfd6ed5c1f6eaba95c8a37895e12a62
Guillermo Gutiérrez <guillermo.gutierrez@donewtech.com>
2011-08-04 17:45:26 +0200

    REFACTOR

--- ggalmazor/python/test/mine_sweeper_tests.py
+++ ggalmazor/python/test/mine_sweeper_tests.py
@@ -3,70 +3,37 @@ import unittest
 EOF = "0 0"
 
 class mine_sweeper_tests(unittest.TestCase):
-    def isHeader(self, line):
-        return line.count(" ") > 0
+
 
     def test_sabe_cuando_hay_un_campo_definido(self):
         fields = "1 1\n.\n0 0"
 
-        field_count = 0
-        for line in fields.split("\n"):
-            if (self.isHeader(line)):
-                if (EOF == line):
-                    break
-                field_count += 1
-                height, width = map(lambda text: int(text), line.split(" "))
+        field_count, height, tiles, width = self.parse_fields(fields)
 
         self.assertEquals(1, field_count)
 
     def test_sabe_el_ancho_y_el_alto_de_un_campo(self):
         fields = "2 3\n...\n...\n0 0"
 
-        field_count = width = height = 0
-        for line in fields.split("\n"):
-            if (self.isHeader(line)):
-                if (EOF == line):
-                    break
-                field_count += 1
-                height, width = map(lambda text: int(text), line.split(" "))
+        field_count, height, tiles, width = self.parse_fields(fields)
 
         self.assertEquals(2, height)
         self.assertEquals(3, width)
 
-    def readTiles(self, line, tiles):
-        for tile in line:
-            tiles.append(tile)
 
     def test_guarda_las_celdas_de_un_campo(self):
         fields = "2 3\n...\n*..\n0 0"
 
-        field_count = width = height = 0
-        tiles = []
-        for line in fields.split("\n"):
-            if (self.isHeader(line)):
-                if (EOF == line):
-                    break
-                field_count += 1
-                height, width = map(lambda text: int(text), line.split(" "))
-            else:
-                self.readTiles(line, tiles)
+        field_count, height, tiles, width = self.parse_fields(fields)
 
         self.assertEquals(6, len(tiles))
         self.assertEquals("*", tiles[3])
 
+
     def test_resuelve_un_campo_de_1x1_sin_minas(self):
         fields = "1 1\n.\n0 0"
 
-        field_count = width = height = 0
-        tiles = []
-        for line in fields.split("\n"):
-            if (self.isHeader(line)):
-                if (EOF == line):
-                    break
-                field_count += 1
-                height, width = map(lambda text: int(text), line.split(" "))
-            else:
-                self.readTiles(line, tiles)
+        field_count, height, tiles, width = self.parse_fields(fields)
 
         solution = "Field #1:" + "\n"
         for y in range(0, height):
@@ -78,9 +45,29 @@ class mine_sweeper_tests(unittest.TestCase):
                 solution += tile
             solution += "\n"
         solution += "\n"
-        
+
         exp_solution = "Field #1:\n0\n\n"
         self.assertEquals(exp_solution, solution)
+
+    def is_header(self, line):
+        return line.count(" ") > 0
+
+    def readTiles(self, line, tiles):
+        for tile in line:
+            tiles.append(tile)
+
+    def parse_fields(self, fields):
+        field_count = width = height = 0
+        tiles = []
+        for line in fields.split("\n"):
+            if (self.is_header(line)):
+                if (EOF == line):
+                    break
+                field_count += 1
+                height, width = map(lambda text: int(text), line.split(" "))
+            else:
+                self.readTiles(line, tiles)
+        return field_count, height, tiles, width
 
 
         
