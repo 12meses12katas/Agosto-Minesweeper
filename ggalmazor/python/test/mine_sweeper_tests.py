6fc563f7938523cec85d759d10709351a0d9270a
Guillermo Gutiérrez <guillermo.gutierrez@donewtech.com>
2011-08-04 17:18:36 +0200

    VERDE

--- ggalmazor/python/test/mine_sweeper_tests.py
+++ ggalmazor/python/test/mine_sweeper_tests.py
@@ -3,7 +3,13 @@ import unittest
 class mine_sweeper_tests(unittest.TestCase):
     def test_sabe_cuando_hay_un_campo_definido(self):
         fields = "1 1\n.\n0 0"
+
         field_count = 0
+        for line in fields.split("\n"):
+            if (line.count(" ") > 0):
+                if ("0 0" != line):
+                    field_count += 1
+
         self.assertEquals(1, field_count)
 
         
