c29eb3e708833c9f6e03d903445b8908aaddd3e3
Angel Luis Jimenez Martinez <soyangel@gmail.com>
2011-08-04 18:12:27 +0200

    Version inicial

---
+++ soyangel/project.clj
@@ -0,0 +1,6 @@
+(defproject minesweeper "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [
+                [org.clojure/clojure "1.2.1"]
+                 [swank-clojure "1.3.2"]
+                ])
