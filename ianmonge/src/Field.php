89a87afad857cb998471a36e062d41bb00a8349d
Ian Monge <ianmonge@gmail.com>
2011-08-25 23:39:44 +0200

    Begin the test of Field class.

--- ianmonge/src/Field.php
+++ ianmonge/src/Field.php
@@ -30,17 +30,60 @@ class Field
      * @param integer $colsNum 
      */
     public function __construct( $rowsNum, $colsNum )
-    {}
+    {
+        $this->rowsNum = $rowsNum;
+        $this->colsNum = $colsNum;
+        
+        for( $row = 0; $row < $this->rowsNum; $row++ )
+        {
+            for( $col = 0; $col < $this->colsNum; $col++ )
+            {
+                $this->board[ $row ][ $col ] = new SquareSimple;
+            }
+        }
+    }
     
     /**
-     * Sets a square in an indicated position.
+     * Sets a square bomb in an indicated position.
      *
      * @param integer $row
      * @param integer $col
-     * @param SquareAbstract $square 
      */
-    public function setSquare( $row, $col, SquareAbstract $square )
-    {}
+    public function setSquareBomb( $row, $col )
+    {
+        $this->board[ $row ][ $col ] = new SquareBomb();
+        
+        $this->updateSquaresNear( $row, $col );
+    }
+    
+    protected function updateSquaresNear( $row, $col )
+    {
+        for( $r = $row-1; $r < $row+2; $r++ )
+        {
+            for( $c = $col-1; $c < $col+2; $c++ )
+            {
+                $this->updateSquare( $r, $c );
+            }
+        }
+    }
+    
+    protected function updateSquare( $row, $col )
+    {
+        // If the square exists.
+        if ( 
+                array_key_exists( $row, $this->board )
+            &&  array_key_exists( $col, $this->board[ $row ] )
+        )
+        {
+            $square = $this->board[ $row ][ $col ];
+            
+            // If the square is not a bomb.
+            if ( $square instanceof SquareSimple )
+            {
+                $square->incrementNumOfBombs();
+            }
+        }
+    }
     
     /**
      * Prints the solution of the field.
